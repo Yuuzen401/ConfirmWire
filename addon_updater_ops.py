a8ff0f919eeab07959bcdf4b5bd2daf40a57cb31
Yuuzen401 <Yuuzen401@gmail.com>
2023-02-14 23:51:37 +0900

    test blender-addon-updater

--- addon_updater_ops.py
+++ addon_updater_ops.py
@@ -1362,7 +1362,7 @@ def register(bl_info):
     # updater.addon = # define at top of module, MUST be done first
 
     # Website for manual addon download, optional but recommended to set.
-    updater.website = "https://github.com/Yuuzen401/ConfirmWire/"
+    updater.website = "https://github.com/Yuuzen401/ConfirmWire"
 
     # Addon subfolder path.
     # "sample/path/to/addon"
@@ -1460,7 +1460,8 @@ def register(bl_info):
     # Note: updater.include_branch_list defaults to ['master'] branch if set to
     # none. Example targeting another multiple branches allowed to pull from:
     # updater.include_branch_list = ['master', 'dev']
-    updater.include_branch_list = None  # None is the equivalent = ['master']
+    # updater.include_branch_list = None  # None is the equivalent = ['master']
+    updater.include_branch_list = ['main', 'dev']
 
     # Only allow manual install, thus prompting the user to open
     # the addon's web page to download, specifically: updater.website
